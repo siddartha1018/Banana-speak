b9c2f9b59e888eea9e567cad37f83d846f38b4ec
siddartha1018 <72271363+siddartha1018@users.noreply.github.com>
2021-01-01 16:56:04 +0530

    taking input

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("This script works")
+var btntranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
+
+btntranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked!!");
+    console.log(txtinput.value);
+});
