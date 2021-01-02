f84b365f9d69e86025d00b380cb9b13ef21c0055
siddartha1018 <72271363+siddartha1018@users.noreply.github.com>
2021-01-02 18:30:13 +0530

    final changes done

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
 
+// var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverurl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text) {
+    return serverurl + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with the server! Wait and please try again later");
+}
+
 btntranslate.addEventListener("click", function clickEventHandler() {
-    outputdiv.innerText = txtinput.value;
+    // input
+    var inputtxt = txtinput.value;
+    //processing data
+    fetch(getTranslationUrl(inputtxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedoutput = json.contents.translated;
+        outputdiv.innerText = translatedoutput;
+    })
+    .catch(errorHandler)
 });
